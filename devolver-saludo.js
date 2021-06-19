60e8f2a96061fb16a77dd99a7da7f10f2f2c5df8
sikitrak <sikitrak@protonmail.com>
2021-06-19 17:03:57 +0200

    Esto es la prueba 2,optimización de funcion javascript.

--- devolver-saludo.js
+++ devolver-saludo.js
@@ -1,5 +1,7 @@
 addEventListener("load",function(){
- welcome="¡Hola Mundo!";
- document.getElementById("encabezado1").innerHTML=welcome; 
-},true);
-
+  welcome="¡Hola Mundo!";
+  var encabezado1=document.getElementById("encabezado1");
+    if(encabezado1!=null){
+    encabezado1.innerHTML=welcome;
+    }
+  },true);
