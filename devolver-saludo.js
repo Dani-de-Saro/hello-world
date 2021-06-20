147461f733aac46b311d9733751c5e53e48e0ade
sikitrak <sikitrak@protonmail.com>
2021-06-20 11:40:51 +0200

    actualización enlace js

--- devolver-saludo.js
+++
@@ -1,7 +0,0 @@
-addEventListener("load",function(){
-  welcome="¡Hola Mundo!";
-  var encabezado1=document.getElementById("encabezado1");
-    if(encabezado1!=null){
-    encabezado1.innerHTML=welcome;
-    }
-  },true);
