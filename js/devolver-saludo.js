1f6c4026befec6f861d23639b1cd73503785aeb2
sikitrak <sikitrak@protonmail.com>
2021-06-20 11:38:11 +0200

    cambio de ubicación

---
+++ js/devolver-saludo.js
@@ -0,0 +1,7 @@
+addEventListener("load",function(){
+  welcome="¡Hola Mundo!";
+  var encabezado1=document.getElementById("encabezado1");
+    if(encabezado1!=null){
+    encabezado1.innerHTML=welcome;
+    }
+  },true);
